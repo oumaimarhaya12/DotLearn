25ca725af94ca45a1ab28172c222ae2b4b3b653c
oumaimarhaya12 <oumaimarhaya7@gmail.com>
2024-11-25 09:09:48 +0100

    firstcommit

---
+++ Controllers/LessonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotLearn.Controllers
+{
+    public class LessonController : Controller
+    {
+        public IActionResult Start()
+        {
+            return View();
+        }
+
+        public IActionResult OverviewLesson()
+        {
+            return View();
+        }
+
+        public IActionResult CoreConcepts()
+        {
+            return View();
+        }
+
+        public IActionResult Architecture()
+        {
+            return View();
+        }
+
+        public IActionResult Implementation()
+        {
+            return View();
+        }
+
+        public IActionResult Challenges()
+        {
+            return View();
+        }
+
+        public IActionResult UseCases()
+        {
+            return View();
+        }
+
+        public IActionResult Congratulations()
+        {
+            return View();
+        }
+    }
+}
